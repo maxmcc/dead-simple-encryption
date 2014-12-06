4715168b248692b8577ecfb359620527f52514b8
William <williamrchr@gmail.com>
2014-12-06 17:26:45 -0500

    splitFiles has code to know next place
    splitFiles now adds data for location of next chunk

--- splitFiles.py
+++ splitFiles.py
@@ -8,22 +8,25 @@ def splitFile(filename):
 
 	counter = 0
 	currBit = 0 #save state of bit
-	for file in next(os.walk(os.getcwd()))[2]:
+	files = next(os.walk(os.getcwd()))[2]
+	for i,file in enumerate(i,files):
 		if (filename != file and file != "splitFiles.py"): #skip if we try to inject in the file were splitting into
 			workingFileSize = os.path.getsize(file)
 			chunkSize = 0.15 * workingFileSize
 			workingFile = open(file, "a+b")
 			workingFile.write("\n ---INJECTED DATA--- \n")
-		
+
 			print "writing to " + file
 		
 			if chunkSize > (fileSize - currBit): #remaining bits left
 				print "last chunk"
+				workingFile.write("next = none")
 				workingFile.write(data[currBit:])
 				workingFile.close()
 				break;
 			else: #have to go to next file
 				print "go to next :("
+				workingFile.write("next = " + files[i+1])
 				workingFile.write(data[currBit:currBit+chunkSize])
 				workingFile.close()
 				currBit+=chunkSize
