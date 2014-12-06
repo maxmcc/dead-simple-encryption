6ee86d4adb094ab8bd8efb9de611544c066a6e0a
Ben Sandler <ben1sandler@gmail.com>
2014-12-06 16:05:15 -0500

    app functional

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def app():
+def main_page():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
