3e817d89ecf74158cfcedbc86d4ae7716efc4a04
Ben Sandler <ben1sandler@gmail.com>
2014-12-06 21:40:32 -0500

    python added to app

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+
+var PythonShell = require('python-shell');
+
+var options = {
+  mode: 'text',
+  args: ['value1', 'value2', 'value3']
+};
+
+PythonShell.run('hello.py', function (err, results) {
+  if (err) throw err;
+  console.log('results: %j', results);
+});
+
   return res.render('index');
 });
 
