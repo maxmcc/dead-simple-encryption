6b34cf3e23b675ba328156736a7f17fbf5e55335
William <williamrchr@gmail.com>
2014-12-07 06:21:45 -0500

    encrypt.py and decrypt.py work!
    encrypt.py takes an arbitrary number of files, zips them, encrypts the
    resulting zip by AES, and then injects these zips into files
    algorithmically determined based on file size and relevance.
    decrypt.py decrypts the resulting zip of encrypt.py and produces an
    output.zip file that can be unzipped and retrieved.

--- file_directory_scripts/splitFiles.py
+++ file_directory_scripts/splitFiles.py
@@ -8,29 +8,27 @@ def split_file(filename):
 
     curr_bit = 0  # save state of bit
     files = next(os.walk(os.getcwd()))[2]
-    files.remove("extractZip.py")
-    files.remove("splitFiles.py")
-    files.remove("output.zip")
     for i, f in enumerate(files):
         if filename != f and f != "splitFiles.py":
             # skip if we try to inject in the file were splitting into
-            working_file_size = os.path.getsize(f)
-            chunk_size = int(0.15 * working_file_size)
-            working_file = open(f, "a+b")
-            working_file.write("\n ---INJECTED DATA--- \n")
-            re_split = open("splitTest.zip", "a+b")
-            if chunk_size >= (file_size - curr_bit):  # last chunk
-                working_file.write("next = none \n")
-                working_file.write(data[curr_bit:])
-                re_split.write(data[curr_bit:])
-                working_file.close()
-                break
-            else:  # have to go to next file
-                if curr_bit == 0:
-                    working_file.write("--- head --- \n")
-                working_file.write("next = " + files[i + 1] + "\n")
-                working_file.write(
-                    data[curr_bit:curr_bit + chunk_size + 1])
-                re_split.write(data[curr_bit:curr_bit + chunk_size + 1])
-                working_file.close()
-                curr_bit += chunk_size
+			working_file_size = os.path.getsize(f)
+			chunk_size = int(0.15 * working_file_size)
+			working_file = open(f, "a+b")
+			working_file.write("\n ---INJECTED DATA--- \n")
+			if curr_bit == 0:
+				working_file.write("--- head --- \n")
+				print "first file: " + f
+            #re_split = open("splitTest.zip", "a+b")
+			if chunk_size >= (file_size - curr_bit):  # last chunk
+				print "last file: " + f
+				working_file.write("next = none \n")
+				working_file.write(data[curr_bit:])
+				# re_split.write(data[curr_bit:])
+				working_file.close()
+				break
+			else:  # have to go to next file
+				working_file.write("next = " + files[i + 1] + "\n")
+				working_file.write(data[curr_bit:curr_bit + chunk_size + 1])
+				#re_split.write(data[curr_bit:curr_bit + chunk_size + 1])
+				working_file.close()
+				curr_bit += chunk_size
