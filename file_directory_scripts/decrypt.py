ac5af579fd9fd5964d21b9fb497eb28fbcd80285
Yoni Nachmany <nachmany@seas.upenn.edu>
2014-12-07 03:28:43 -0500

    Decrypt filled in

--- file_directory_scripts/decrypt.py
+++ file_directory_scripts/decrypt.py
@@ -3,5 +3,6 @@ import AES_encryption
 def decrypt(file_name, password):
 	unzip(file_name)
 	extractZip()
-	# key = makeKey(password)
-	# decrypt("output.zip", key)
+	key = makeKey(password)
+	decrypt("output.zip", key)
+	unzip("output.zip")
