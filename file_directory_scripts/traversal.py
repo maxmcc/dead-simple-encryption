6c47bac68f818fb8975f68ff73f4e8bf68845151
William <williamrchr@gmail.com>
2014-12-07 02:01:51 -0500

    added else statement for traversal.py

--- file_directory_scripts/traversal.py
+++ file_directory_scripts/traversal.py
@@ -56,6 +56,25 @@ def gssd(root_path, desired_size):
 				return child
 
 		return root_path
+	else: 
+				if getFolderSize(root_path) < desired_size:
+			return None
+
+		children = map(lambda child: os.path.abspath(child), os.listdir(root_path))
+		no_dots = filter(lambda child: os.path.basename(child)[0] is not '.', children)
+		no_files = filter(lambda child: os.path.isdir(child), no_dots)
+
+		def cmp_by_size(a, b):
+			a_size = os.path.getsize(a)
+			b_size = os.path.getsize(b)
+			return cmp(a_size, b_size)
+
+		sorted_children = sorted(no_files, cmp=cmp_by_size)
+		for child in sorted_children:
+			if gssd(child, desired_size) is not None:
+				return child
+
+		return root_path
 	
 print gssd("D:/", 1024*1024*1024)
 print 1024*1024*1024
